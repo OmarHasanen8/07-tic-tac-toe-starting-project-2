5c50d1441a365ed3ca0179932d54f6ef9ee0954a
Omar hasanen <omarhasanen923@gmail.com>
2025-10-09 22:39:16 +0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/07-tic-tac-toe-starting-project-2/', // 👈 هذا مهم جداً
 })
