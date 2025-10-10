f69bb94bf1b76d61c5f8da84fc4a0650536f4bc2
Omar hasanen <omarhasanen923@gmail.com>
2025-10-10 18:34:02 +0300

    Add player components and make it reusable

---
+++ src/components/player.jsx
@@ -0,0 +1,13 @@
+export default function Player({name , symbol}) {
+
+return (<li> 
+  <span className="Player">
+  <span className="player-name"> {name}  </span> 
+<span className="player-symbol">  {Symbol} </span>
+</span> 
+<button> Edit </button>  
+</li>
+
+)
+
+}
