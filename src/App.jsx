9c1fa9c6f5f725cd3b2705109b5ef6f16602eee2
Omar hasanen <omarhasanen923@gmail.com>
2025-10-10 17:58:30 +0300

    Add the Game Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 
 function App() {
   
-return ( 
-<p> Coming Soon  </p>
+return <main> 
+<div id="game-container"> 
+<ol id="players"> 
+<li> <span className="player-name"> player1  </span> 
+<span className="player-symbol">  X </span></li>
+<li > 
+<span className="player-name"> player2 </span> 
+<span className="player-symbol">  O </span> </li>
+</ol>
+GAME BOARD
+
+</div>
 
 
-) 
+</main>
+
+
+
 
   }
 
